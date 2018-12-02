51fce8b3b5d1461b2bcc43e5fb08dfddae282edf
Christopher Czach <chrisczach@gmail.com>
2018-12-01 22:38:15 -0800

    add DLL methods

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -16,16 +16,17 @@ class DoublyLinkedList {
   push(val) {
     const node = new Node(val, null, this.tail);
     this.length++;
-    if (!this.head) {return !! (this.head = this.tail = node);}
+    if (!this.head) { return !!(this.head = this.tail = node); }
     this.tail.next = node;
-    this.tail  = node;
+    this.tail = node;
     return true;
   }
 
-  pop(){
+  pop() {
     const popped = this.tail;
-    if(!this.head) {return false
-    } else if(this.length === 1) {
+    if (!this.head) {
+      return false
+    } else if (this.length === 1) {
       this.head = this.tail = null;
     } else {
       this.tail = this.tail.prev;
@@ -34,6 +35,74 @@ class DoublyLinkedList {
     popped.prev = null;
     this.length--;
     return popped;
+  }
+
+  shift() {
+    const shifted = this.head;
+    if (!this.head) {
+      return false
+    } else if (this.length === 1) {
+      this.head = this.tail = null;
+    } else {
+      this.head = this.head.next
+      this.head.prev = null;
+    }
+    shifted.next = null;
+    this.length--;
+    return shifted;
+  }
+
+  unshift(val) {
+    if (val === undefined) { return false }
+    const node = new Node(val);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+    return true;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return false;
+    let node;
+    if (index <= this.length / 2) {
+      node = this.head
+      for (let i = 0; i < index; i++) {
+        node = node.next;
+      }
+    } else {
+      node = this.tail;
+      for (let i = this.length; i > index; i--) {
+        node = node.prev;
+      }
+    }
+    return node;
+  }
+
+  set(index, val) {
+    const node = this.get(index);
+    if (node === false) { return false };
+    node.val = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) {
+      return false
+    } else if (index === 0) {
+      return this.unshift(val)
+    } else if (index === this.length) {
+      return this.push(val)
+    } else {
+      const node = new Node(val);
+      const insertPoint = this.get(index);
+      if (insertPoint === false) { return false };
+      node.next = insertPoint.next;
+      node.prev = insertPoint;
+      insertPoint.next = node;
+      node.next.prev = node;
+      this.length++;
+      return true;
+    }
   }
 }
 
