d333954529922ee729d833cf28f10163de972979
Chris Czach <chrisczach@gmail.com>
2018-12-05 10:23:38 -0800

    add edge

--- graph.js
+++ graph.js
@@ -9,5 +9,26 @@ class Graph {
       : (this.adjacencyList[value] = []);
   }
 
+  addEdge(v1, v2) {
+    const list = this.adjacencyList;
+    if(!list[v1] || !list[v2]) {
+      const missing = !list[v1] && !list[v2]  ?  `${v1} and ${v2} are ` : !list[v1]  ?  `${v1} is` :  `${v2} is`;
+      return `Vertex ${missing} invalid`
+    }
+    if(list[v1].indexOf(v2)  !== -1 )  {
+      return `${v1} and ${v2} edge already exist`
+    }
+
+    list[v1].push(v2);
+    list[v2].push(v2);
+    return `${v1} and ${v2} edge added`
+  }
+
   
 }
+
+const graph = new Graph();
+graph.addVertex('a');
+graph.addVertex('b');
+graph.addVertex('c');
+graph.addEdge('a', 'b');
